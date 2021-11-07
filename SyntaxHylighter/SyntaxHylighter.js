ff214a0cbb5a54dadee3800ae467ed639a577f63
chandu-programmer <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-11-07 21:24:07 +0530

    updated code

--- SyntaxHylighter/SyntaxHylighter.js
+++ SyntaxHylighter/SyntaxHylighter.js
@@ -23,6 +23,7 @@ window.onload = function () {
         '-',
         '*',
         '/',
+        '.'
     ]
     const jsUnaryOperators = [
         '==',
@@ -41,6 +42,9 @@ window.onload = function () {
         'const'
     ]
 
+    const jsFunc = ['(', ')']
+    const jsString = ['"', "'", '`']
+
     let tokens = []
     code.innerHTML.split(' ').forEach(token => {
         if (token != '')
@@ -48,20 +52,38 @@ window.onload = function () {
         else { }
     })
 
+    let trimmedToken = []
     let trimmedTokens = []
+    let operators = []
     let isOperator;
 
     code.innerHTML = ''
+
+    TrimTokens()
+
+
+    trimmedToken.forEach(trimmedTok => {
+        if (!Array.isArray(trimmedTok)) {
+            trimmedTokens.push(trimmedTok)
+        } else if (Array.isArray(trimmedTok)) {
+            trimmedTok.forEach(trimTok => {
+                trimmedTokens.push(trimTok)
+            })
+        }
+    })
+    console.log(trimmedTokens)
+
     switch (code.lang) {
         case 'js':
-            JSSyntaxHighlight(tokens);
+            JSSyntaxHighlight(trimmedTokens);
             break
     }
 
+    //main fucntion that highlightes the js code
     function JSSyntaxHighlight(tokens) {
         for (i = 0; i <= tokens.length; i++) {
             let token = tokens[i]
-            if (token == undefined) {
+            if (token == undefined || token == '') {
                 continue
             }
             switch (token) {
@@ -71,13 +93,76 @@ window.onload = function () {
                 case jsTokens[jsTokens.indexOf(token)]:
                     code.innerHTML += `<span class="sh-token">${token} </span>`
                     break
+                case jsTernaryOperators[jsTernaryOperators.indexOf(token)]:
+                    code.innerHTML += `<span class="sh-operator">${token} </span>`
+                    break
+                case jsUnaryOperators[jsUnaryOperators.indexOf(token)]:
+                    code.innerHTML += `<span class="sh-operator">${token} </span>`
+                    break
                 default:
                     if (!/[a-z]/.test(token.charAt(0)) && /[A-Z]/.test(token.charAt(0)))
                         code.innerHTML += `<span class="sh-class">${token} </span>`
+                    else if (!/[a-z]/.test(token) && !/[A-Z]/.test(token))
+                        code.innerHTML += `<span class="sh-operator">${token} </span>`
                     else
                         code.innerHTML += `<span class="sh-variable">${token} </span>`
                     break
             }
         }
+    } // end of the main js highlighting function
+
+    //fucntion that trims the tokens furthur to detect the other operators like arthematic operators etc.
+    function TrimTokens() {
+        for (k = 0; k <= tokens.length; k++) {
+            let token = tokens[k]
+            let tok
+            if (token == undefined || token == '')
+                return
+            if (token.length > 1) {
+                for (n = 0; n <= token.length; n++) {
+                    if (token[n] != undefined) {
+                        if (jsOperators.includes(token[n])) {
+                            tok = token[n]
+                            isOperator = true
+                            operators.push(tok)
+                            break
+                        } else if (jsFunc.includes(token[n])) {
+                            tok = token[n]
+                            isOperator = true
+                            operators.push(tok)
+                            break
+                        } else {
+                            isOperator = false
+                        }
+                    }
+                }
+                if (isOperator)
+                    IdentiyTokens(token, tok)
+                else
+                    PushToken(token)
+            }
+            else
+                PushToken(token)
+        }
+    }
+
+    function IdentiyTokens(token, symbol) {
+        let splitted = token.split(symbol)
+        for (m = 0; m <= splitted.length; m++) {
+            console.log(splitted[m])
+            if (m + 1 % 2 == 0) {
+                console.log(symbol)
+                trimmedToken.push(splitted[m])
+            } else if (m % 2 != 0) {
+                trimmedToken.push(symbol)
+            } else {
+                trimmedToken.push(splitted[m])
+            }
+        }
+        isOperator = false
+    }
+
+    function PushToken(token) {
+        trimmedToken.push(token)
     }
 }
