83ab5bcbc3498f6bba62630292dffccfc041b2e6
chandu-programmer <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-11-08 21:26:25 +0530

    updated code

--- SyntaxHylighter/SyntaxHylighter.js
+++ SyntaxHylighter/SyntaxHylighter.js
@@ -23,8 +23,10 @@ window.onload = function () {
         '-',
         '*',
         '/',
-        '.'
+        '.',
     ]
+
+    const jsEOL = [';', '\n']
     const jsUnaryOperators = [
         '==',
         '++',
@@ -39,7 +41,8 @@ window.onload = function () {
     ]
     const jsKeywords = [
         'var',
-        'const'
+        'const',
+        'let'
     ]
 
     const jsFunc = ['(', ')']
@@ -59,19 +62,17 @@ window.onload = function () {
 
     code.innerHTML = ''
 
-    TrimTokens()
-
+    TrimTokens(tokens)
+    console.log(trimmedToken)
 
     trimmedToken.forEach(trimmedTok => {
         if (!Array.isArray(trimmedTok)) {
             trimmedTokens.push(trimmedTok)
         } else if (Array.isArray(trimmedTok)) {
-            trimmedTok.forEach(trimTok => {
-                trimmedTokens.push(trimTok)
-            })
+            console.log("working")
+            TrimTokens(trimmedTok)
         }
     })
-    console.log(trimmedTokens)
 
     switch (code.lang) {
         case 'js':
@@ -85,7 +86,7 @@ window.onload = function () {
             let token = tokens[i]
             if (token == undefined || token == '') {
                 continue
-            }
+            }else
             switch (token) {
                 case jsKeywords[jsKeywords.indexOf(token)]:
                     code.innerHTML += `<span class="sh-keyword">${token} </span>`
@@ -94,13 +95,36 @@ window.onload = function () {
                     code.innerHTML += `<span class="sh-token">${token} </span>`
                     break
                 case jsTernaryOperators[jsTernaryOperators.indexOf(token)]:
-                    code.innerHTML += `<span class="sh-operator">${token} </span>`
+                    code.innerHTML += `<span class="sh-operator">${token}</span>`
                     break
                 case jsUnaryOperators[jsUnaryOperators.indexOf(token)]:
-                    code.innerHTML += `<span class="sh-operator">${token} </span>`
+                    code.innerHTML += `<span class="sh-operator">${token}</span>`
+                    break
+                case jsEOL[jsEOL.indexOf(token)]:
+                    code.innerHTML += `<span class="sh-operator">${token}</span><br>`
                     break
                 default:
-                    if (!/[a-z]/.test(token.charAt(0)) && /[A-Z]/.test(token.charAt(0)))
+                    if (!isNaN(token))
+                        code.innerHTML += `<span class="sh-numerals">${token}</span>`
+                    else if (token.includes('"') || token.includes("'") || token.includes('`')) {
+                        code.innerHTML += `<span class="sh-string">${token} </span>`
+                        if (jsString.includes(token.charAt(0)) && jsString.includes(token.charAt(token.length - 1))) { }
+                        else
+                        for (i; i < tokens.length; i++) {
+                            let toke = tokens[i + 1]
+                            if (toke == undefined) { }
+                            else
+                            if (toke.includes('"') || toke.includes("'") || toke.includes('`')) {
+                                code.innerHTML += `<span class="sh-string">${toke} </span>`
+                                i++
+                                break
+                            } else {
+                                code.innerHTML += `<span class="sh-string">${toke} </span>`
+                                continue
+                            }
+                        }
+                    }
+                    else if (!/[a-z]/.test(token.charAt(0)) && /[A-Z]/.test(token.charAt(0)))
                         code.innerHTML += `<span class="sh-class">${token} </span>`
                     else if (!/[a-z]/.test(token) && !/[A-Z]/.test(token))
                         code.innerHTML += `<span class="sh-operator">${token} </span>`
@@ -112,7 +136,7 @@ window.onload = function () {
     } // end of the main js highlighting function
 
     //fucntion that trims the tokens furthur to detect the other operators like arthematic operators etc.
-    function TrimTokens() {
+    function TrimTokens(tokens) {
         for (k = 0; k <= tokens.length; k++) {
             let token = tokens[k]
             let tok
@@ -127,6 +151,11 @@ window.onload = function () {
                             operators.push(tok)
                             break
                         } else if (jsFunc.includes(token[n])) {
+                            tok = token[n]
+                            isOperator = true
+                            operators.push(tok)
+                            break
+                        } else if (jsEOL.includes(token[n])) {
                             tok = token[n]
                             isOperator = true
                             operators.push(tok)
@@ -148,15 +177,13 @@ window.onload = function () {
 
     function IdentiyTokens(token, symbol) {
         let splitted = token.split(symbol)
-        for (m = 0; m <= splitted.length; m++) {
-            console.log(splitted[m])
-            if (m + 1 % 2 == 0) {
-                console.log(symbol)
-                trimmedToken.push(splitted[m])
+        for (m = 0; m < splitted.length + 1; m++) {
+            if (m == 0)
+                trimmedToken.push(splitted[0])
+            else if (m % 2 == 0) {
+                trimmedToken.push(splitted[m - 1])
             } else if (m % 2 != 0) {
                 trimmedToken.push(symbol)
-            } else {
-                trimmedToken.push(splitted[m])
             }
         }
         isOperator = false
